4fa489415e658b18d4c621c1976c0ab529599969
Taemin Hong <taeminhong@outlook.com>
2019-02-28 10:49:12 +0900

    Add an example app depends on DirectX

---
+++ configure.bat
@@ -0,0 +1,18 @@
+@for %%P in (x86 x64) do (
+	@for %%C in (Debug Release) do (
+		@mkdir bin\%%P\%%C
+	)
+)
+
+@rem DirectX Feb 2010
+@if not exist .\vendors\DirectXFeb2010 (
+	@if not exist .\downloads\DirectXFeb2010.zip (
+		@.\tools\wget -O .\downloads\DirectXFeb2010.zip https://www.taemin.site/packages/DirectX/DirectXFeb2010.zip
+	)
+	@unzip -d vendors\DirectXFeb2010 -o .\downloads\DirectXFeb2010.zip
+)
+@for %%P in (x86 x64) do (
+	@robocopy vendors\DirectXFeb2010\bin\%%P bin\%%P\Debug
+	@robocopy vendors\DirectXFeb2010\bin\%%P bin\%%P\Release	
+	@copy /Y vendors\DirectXFeb2010\dxwebsetup.exe bin\%%P\Release
+)
